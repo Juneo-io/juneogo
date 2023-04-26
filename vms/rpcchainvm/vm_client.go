8b3efd67c9bc120001001bf7f7ea01cec5ab1701
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-04-26 18:24:23 +0200

    Fixed wrong ctx

--- vms/rpcchainvm/vm_client.go
+++ vms/rpcchainvm/vm_client.go
@@ -204,7 +204,7 @@ func (vm *VMClient) Initialize(
 		XChainId:     chainCtx.XChainID[:],
 		CChainId:     chainCtx.CChainID[:],
 		AvaxAssetId:  chainCtx.AVAXAssetID[:],
-		ChainAssetId: ctx.ChainAssetID[:],
+		ChainAssetId: chainCtx.ChainAssetID[:],
 		ChainDataDir: chainCtx.ChainDataDir,
 		GenesisBytes: genesisBytes,
 		UpgradeBytes: upgradeBytes,
