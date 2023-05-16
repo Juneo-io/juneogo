6d940a5e538c841920aefff1a3fca52edb35136d
Aleksander Waslet <alekswaslet@hotmail.fr>
2023-05-16 18:55:42 +0200

    Updated testnet beacons

--- genesis/beacons.go
+++ genesis/beacons.go
@@ -14,7 +14,15 @@ func getIPs(networkID uint32) []string {
 	case constants.MainnetID:
 		return nil
 	case constants.TestnetID:
-		return nil
+		return []string{
+			"65.108.7.242:9651",
+			"65.108.15.231:9651",
+			"38.242.232.146:9651",
+			"65.20.115.188:9651",
+			"172.104.142.98:9651",
+			"172.104.150.114:9651",
+			"172.104.251.97:9651",
+		}
 	default:
 		return nil
 	}
@@ -26,7 +34,15 @@ func getNodeIDs(networkID uint32) []string {
 	case constants.MainnetID:
 		return nil
 	case constants.TestnetID:
-		return nil
+		return []string{
+			"NodeID-8pi4CuccGXxg7b1BiVFez78cC75zts7qy",
+			"NodeID-5SngxwT4WZV5xCkzYTGqUZWq5jybJmkSN",
+			"NodeID-5bziGPQUGgM6c1d24yCPgtriw7dW2uQUt",
+			"NodeID-3G1TKgx8yxLLoAAcgd4DUG19bYX9A6xdL",
+			"NodeID-B2GHMQ8GF6FyrvmPUX6miaGeuVLH9UwHr",
+			"NodeID-KprXcvbyJv54Fq36N3fGFwjoAMUgSMB7b",
+			"NodeID-28ZM9fn2A23hLkTzzgmkumPtYUnf3zRSV",
+		}
 	default:
 		return nil
 	}
