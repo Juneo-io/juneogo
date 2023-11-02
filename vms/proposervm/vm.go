e443d12f8ca4a2d859bd049c4bdf249d7969750a
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2023-11-02 11:25:37 +0100

    Updated fork height for JuneoMCN

--- vms/proposervm/vm.go
+++ vms/proposervm/vm.go
@@ -738,9 +738,9 @@ func (vm *VM) getForkHeight() (uint64, error) {
 	case constants.PlatformChainID:
 		switch vm.ctx.NetworkID {
 		case constants.MainnetID:
-			return 805732, nil // https://subnets.avax.network/p-chain/block/805732
+			return 1, nil // https://subnets.avax.network/p-chain/block/805732
 		case constants.TestnetID:
-			return 47529, nil // https://subnets-test.avax.network/p-chain/block/47529
+			return 1, nil // https://subnets-test.avax.network/p-chain/block/47529
 		}
 	case mainnetXChainID:
 		return 1, nil // https://subnets.avax.network/x-chain/block/1
